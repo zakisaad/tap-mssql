b64abe0a646b3a6c11d2750e8abbdf7b3dae7e76
Zaki Saad <zaki0saad@gmail.com>
2022-04-26 16:17:53 +1000

    Update incremental.clj
    Add crude support for byte-array-based MSSQL timestamps (rowversion)

--- src/tap_mssql/sync_strategies/incremental.clj
+++ src/tap_mssql/sync_strategies/incremental.clj
@@ -8,7 +8,37 @@
             [clojure.tools.logging :as log]
             [clojure.string :as string]
             [clojure.java.jdbc :as jdbc]))
-
+ 
+;; - jwc: reference to send off pure hexadecimal formatted rowversion to mssql, if required
+;; (:import [java.lang Integer]))
+ 
+;; (defn signed-int-to-hex
+;;   [unsigned-int]
+;;   (take-last 2 (str "0" (java.lang.Integer/toHexString (if (< unsigned-int 0) (+ unsigned-int 256) unsigned-int)))))
+ 
+;; (defn build-incremental-sync-query-hex-rowversion
+;;   [stream-name schema-name table-name record-keys replication-key state]
+;;   {:pre [(not (empty? record-keys))]} ;; Is there more incoming state that we think is worth asserting?
+;;   (let [replication-key-name (get-in state ["bookmarks" stream-name "replication_key_name"])
+;;         replication-key-value (get-in state ["bookmarks" stream-name "replication_key_value"])
+;;         add-where-clause?     (and (some? replication-key-value)
+;;                                    (= replication-key replication-key-name)) ;; if the replication-key in metadata changes, we negate our bookmark
+;;         bookmarking-clause    (format "%s >= %s" (common/sanitize-names replication-key) (str "0x" (apply str (reduce (fn [all single] ( concat all (signed-int-to-hex single))) "" replication-key-value))))
+;;         where-clause          (when add-where-clause?
+;;                                 (str " WHERE " bookmarking-clause))
+;;         order-by              (str " ORDER BY " (common/sanitize-names replication-key))
+;;         sql-params            [(str (format "SELECT %s FROM %s.%s"
+;;                                             (string/join ", " (map common/sanitize-names record-keys))
+;;                                             (common/sanitize-names schema-name)
+;;                                             (common/sanitize-names table-name))
+;;                                     where-clause
+;;                                     order-by)]]
+;;     (if add-where-clause?
+;;       (concat sql-params
+;;               [  ])
+;;       sql-params)))
+;; + jwc
+ 
 (defn build-incremental-sync-query
   [stream-name schema-name table-name record-keys replication-key state]
   {:pre [(not (empty? record-keys))]} ;; Is there more incoming state that we think is worth asserting?
@@ -28,9 +58,10 @@
                                     order-by)]]
     (if add-where-clause?
       (concat sql-params
-              [replication-key-value])
-      sql-params)))
-
+              (if (= replication-key-name "timestamp")
+                [ (biginteger (byte-array replication-key-value)) ]
+                [replication-key-value]))
+      sql-params))) 
 (defn sync-and-write-messages!
   "Syncs all records, states, returns the latest state. Ensures that the
   bookmark we have for this stream matches our understanding of the fields
